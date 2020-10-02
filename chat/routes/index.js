0af38ede3b4748bfbdb1b45d326e2b8a2fb96286
camila0216 <c.gomezs@uniandes.edu.co>
2020-10-01 21:57:17 -0500

    arreglo delete

--- chat/routes/index.js
+++ chat/routes/index.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 const Joi = require("joi");
 const { Timestamp } = require("mongodb");
+const { date } = require("joi");
 
 router.get("/", function (req, res, next) {
   msg.getMsgs((msgs) => {
@@ -49,43 +50,43 @@ router.post("/", function (req, res, next) {
 });
 
 router.put("/:ts", function (req, res, next) {
-  msg.getMsg(parseInt(req.params.ts), (msg) => {
-    if (!msg)
+  msg.getMsg(parseInt(req.params.ts), (msg_found) => {
+    if (!msg_found)
       return res.status(404).send("El mensaje con el ts dado no existe");
-    res.send(msg);
-  });
+    else {
+      const schema = Joi.object({
+        message: Joi.string().min(5).required(),
+        author: Joi.string()
+          .pattern(/^[a-z A-Z]+[ ][a-z A-Z]+$/)
+          .required(),
+      });
 
-  const schema = Joi.object({
-    message: Joi.string().min(5).required(),
-    author: Joi.string()
-      .pattern(/^[a-z A-Z]+[ ][a-z A-Z]+$/)
-      .required(),
-    ts: Joi.number().required(),
+      const { error } = schema.validate(req.body);
+      if (error) {
+        console.log(error);
+        return res.status(400).send(error);
+      } else {
+        let date = new Date();
+        const newMsg = {
+          message: req.body.message,
+          author: req.body.author,
+          ts: date.getTime(),
+        };
+        msg.updateMsg(
+          parseInt(req.params.ts),
+          newMsg.message,
+          newMsg.author,
+          newMsg.ts
+        );
+        res.send("Updated");
+      }
+    }
   });
-
-  const { error } = schema.validate(req.body);
-  if (error) {
-    console.log(error);
-    return res.status(400).send(error);
-  } else {
-    const newMsg = {
-      message: req.body.message,
-      author: req.body.author,
-      ts: req.body.ts,
-    };
-    msg.updateMsg(
-      parseInt(req.params.ts),
-      newMsg.message,
-      newMsg.author,
-      newMsg.ts
-    );
-    res.send("Updated");
-  }
 });
 
 router.delete("/:ts", function (req, res, next) {
-  msg.getMsg(parseInt(req.params.ts), (msg) => {
-    if (!msg)
+  msg.getMsg(parseInt(req.params.ts), (msg_found) => {
+    if (!msg_found)
       return res.status(404).send("El mensaje con el ts dado no existe");
     msg.deleteMsg(parseInt(req.params.ts));
     res.send("Deleted");
